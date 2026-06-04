dfde4744e3c347fb7c0b93676bb7d743c85fce3a
alstocras <alstoperiment@gmail.com>
2026-06-04 18:25:34 +0000

    very broken commit

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 import csv
 
 dataCSV = input("Provide the path to the .csv file with the data: ")
-
-reader = csv.DictReader(open(dataCSV))
+dataDict = {}
+reader = csv.DictReader(open(dataCSV), fieldnames=(
+    "name", "id", "centre", "designation", "joinDate", "bank", "bankAccNum", "leaveBalance", "sickLeaves", "addedLeaves", "totalLeaveBalance", "lopDays", "basicSal", "hra", "travelAllowance", "bonus", "otPay", "totPay", "advanceIncome", "advanceDeduction", "advancePending", "netPay", "comments"))
+for row in reader:
+    print(row)

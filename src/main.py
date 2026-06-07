cf38dd2062b99fb461705c88cadea310cc9d559f
alstocras <alstoperiment@gmail.com>
2026-06-07 16:25:29 +0000

    broken commit

--- src/main.py
+++ src/main.py
@@ -3,10 +3,16 @@ from pdfrw import PdfReader
 from pdfrw.buildxobj import pagexobj
 from pdfrw.toreportlab import makerl
 
+outfile = "generations/payslip.pdf"
 
-dataCSV = input("Provide the path to the .csv file with the data: ")
-dataDict = {}
-reader = csv.DictReader(open(dataCSV), fieldnames=(
-    "name", "id", "centre", "designation", "joinDate", "bank", "bankAccNum", "leaveBalance", "sickLeaves", "addedLeaves", "totalLeaveBalance", "lopDays", "basicSal", "hra", "travelAllowance", "bonus", "otPay", "totPay", "advanceIncome", "advanceDeduction", "advancePending", "netPay", "comments"))
-for row in reader:
-    print(row)
+template = PdfReader("assets/template.pdf", decompress=False).pages[0]
+templateObj = pagexobj(template)
+
+canvas = Canvas(outfile)
+
+objName = makerl(canvas, templateObj)
+canvas.doForm(objName)
+
+canvas.drawString(200, 298, "no")
+
+canvas.save()
